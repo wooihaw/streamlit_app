8f2f2e2e6c058839b9ebf5020953bb703a938bc3
wooihaw <wooihaw.tan@gmail.com>
2021-11-06 17:06:04 +0800

    Revised to use CSV module

--- train_model.py
+++ train_model.py
@@ -1,21 +1,36 @@
+import csv
 from sklearn.model_selection import train_test_split as split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
-from pandas import read_csv
 from joblib import dump
 
-df = read_csv('../data/genders_heights_weights.csv')
-X =  df.values[:, 1:]
-y = df.values[:, 0]
+csv_fname = "heights_weights_genders.csv"
 
-X_train, X_test, y_train, y_test = split(X, y, stratify=y, random_state=42)
+# reading csv file using csv module
+with open(csv_fname, "r") as csv_file:
+    reader = csv.reader(csv_file, delimiter=",", quotechar='"')
+    header = next(reader)
+    print(header)
+    data = {}
+    for row in reader:
+        for key, field in zip(header, row):
+            data[key] = data.get(key, []) + [field]
+
+X = [[i, j] for i, j in zip(data["Height(cm)"], data["Weight(kg)"])]
+y = data["Gender"]
+X_train, X_test, y_train, y_test = split(
+    X, y, stratify=y, test_size=0.3, random_state=42
+)
 
 params = {}
-params['C'] = [0.001, 0.01, 0.1, 1, 10, 100]
-gs = GridSearchCV(LogisticRegression(), params, n_jobs=-1, verbose=1).fit(X_train, y_train)
+params["C"] = [0.001, 0.01, 0.1, 1, 10, 100]
+params["penalty"] = ["l1", "l2"]
+gs = GridSearchCV(LogisticRegression(), params, n_jobs=-1, verbose=1).fit(
+    X_train, y_train
+)
 
 print(gs.best_params_, gs.best_score_)
 
 lgr = LogisticRegression(**gs.best_params_).fit(X_train, y_train)
-print(lgr.score(X_test, y_test))
+print(f"{lgr.score(X_test, y_test)=}")
 
-dump(lgr, 'lgr_model.job')
+dump(lgr, "lgr_model.job")
