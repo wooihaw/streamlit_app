2410502cc671e7fbf4d0002c9500818a736f230d
wooihaw <wooihaw.tan@gmail.com>
2024-04-09 12:10:25 +0800

    Updated to use Pandas

--- train_model.py
+++ train_model.py
@@ -1,25 +1,13 @@
-import csv
+from pandas import read_csv
 from sklearn.model_selection import train_test_split as split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from joblib import dump
 
-csv_fname = "heights_weights_genders.csv"
-
-# reading csv file using csv module
-with open(csv_fname, "r") as csv_file:
-    reader = csv.reader(csv_file, delimiter=",", quotechar='"')
-    header = next(reader)
-    print(header)
-    data = {}
-    for row in reader:
-        for key, field in zip(header, row):
-            data[key] = data.get(key, []) + [field]
-
-X = [[float(i), float(j)] for i, j in zip(data["Height(cm)"], data["Weight(kg)"])]
-y = data["Gender"]
-X_train, X_test, y_train, y_test = split(
-    X, y, stratify=y, test_size=0.3, random_state=42
-)
+csv_file = "https://raw.githubusercontent.com/wooihaw/datasets/main/heights_weights_genders.csv"
+df = read_csv(csv_file)
+X = df.drop("Gender", axis=1)
+y = df["Gender"]
+X_train, X_test, y_train, y_test = split(X, y, stratify=y, test_size=0.3, random_state=42)
 
 params = {}
 params["C"] = [0.001, 0.01, 0.1, 1, 10, 100]
@@ -32,6 +20,6 @@ gs = GridSearchCV(LogisticRegression(), params, n_jobs=-1, verbose=1).fit(
 print(gs.best_params_, gs.best_score_)
 
 lgr = LogisticRegression(**gs.best_params_).fit(X_train, y_train)
-print(f"{lgr.score(X_test, y_test)=}")
+print(f"{lgr.score(X_test, y_test)=:.3%}")
 
 dump(lgr, "lgr_model.job")
